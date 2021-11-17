aa377b69c13fb62150306a41e32b8e7b62ad9571
Julio Cesar <jcesar_dlr@hotmail.com>
2021-11-16 21:51:11 -0400

    Mostrando notificaciones de peticiones

--- src/app/pages/heroe/heroe.component.ts
+++ src/app/pages/heroe/heroe.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { HeroeModel } from '../../models/heroe.model';
 import { NgForm } from '@angular/forms';
 import { HeroesService } from '../../services/heroes.service';
+import Swal from 'sweetalert2';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-heroe',
@@ -24,25 +26,34 @@ export class HeroeComponent implements OnInit {
       return; 
     }
 
+    Swal.fire({
+      title: 'Espere',
+      text: 'Guardando información',
+      icon: 'info',
+      allowOutsideClick: false
+    });
+
+    Swal.showLoading();
+
+    let peticion: Observable<any>;
+
+
     if ( this.heroe.id ) {
-
-      this.heroesService.actualizarHeroe( this.heroe )
-        .subscribe( resp => {
-          console.log( resp );
-        });
-
+      peticion = this.heroesService.actualizarHeroe( this.heroe );
     } else {
-
-      this.heroesService.crearHeroe( this.heroe )
-        .subscribe( resp => {
-          console.log( resp );
-          this.heroe = resp; // esto esta demas
-        } );
+      peticion = this.heroesService.crearHeroe( this.heroe );
     }
+
+    peticion.subscribe( resp => {
+
+      Swal.fire({
+        title: this.heroe.nombre,
+        text: 'Se actulizó correctamente',
+        icon: 'success'
+      });
+
+    });
     
-
   }
-
-
 
 }
